330d3da85413e93581570ff53a3b94f581153eb4
dagobertoaltamar <programador1@nivus.co>
2022-12-03 14:32:39 -0500

    Savemoney

--- src/app/context/iMoney.ts
+++ src/app/context/iMoney.ts
@@ -1,14 +1,10 @@
 export interface iMoney {
    currency:MoneyData[] 
 }
-
-
 export interface MoneyData {
    id:string;
    value:string;
 }
-
-
 export interface iAction {
   type:string;
   payload:any
